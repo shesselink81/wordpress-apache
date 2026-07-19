79ef98470131e5fda1517a9371d2a35506b5580b
Sander Hesselink <sander@hesselink.me>
2026-07-19 17:56:39 +0200

    dsad

--- create_tag.ps1
+++ create_tag.ps1
@@ -1,4 +1,4 @@
 $version="7.0.2"
-git tag -a v$version -m "Release version $version"
-git push origin v$version
-echo "Tag v$version created and pushed to origin."
+git tag -a $version -m "Release version $version"
+git push origin $version
+echo "Tag $version created and pushed to origin."
